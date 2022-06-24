e5a0beb19643e955cebcc50406bd289c23a0f8fe
danieljonesobot <daniel.jonesobot@gmail.com>
2022-06-24 12:35:02 +0100

    uploading files and second commit

---
+++ print.js
@@ -0,0 +1,25 @@
+// alert("Hi there and welcome");
+
+let firstName = prompt("Please enter your First Name");
+
+console.log(firstName);
+
+
+let lastName = prompt("Please enter your Last Name");
+
+console.log(lastName);
+
+
+let height = prompt("Please enter your Height");
+
+console.log(height);
+
+let country = prompt("Please enter your Country");
+
+console.log(country);
+
+
+
+// let num = parseInt(prompt("please enter a number"));
+
+// console.log(num);
