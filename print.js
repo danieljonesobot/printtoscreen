6604444eb09bdc326122db0be06bbdbd181b6615
danieljonesobot <daniel.jonesobot@gmail.com>
2022-06-24 13:08:17 +0100

    third commit

---
+++ print.js
@@ -0,0 +1,20 @@
+// alert("Hi there and welcome");
+
+let firstName = prompt("Please enter your First Name");
+
+console.log(firstName);
+
+
+let lastName = prompt("Please enter your Last Name");
+
+console.log(lastName);
+
+
+let height = prompt("Please enter your Height");
+
+console.log(height);
+
+let country = prompt("Please enter your Country");
+
+console.log(country);
+
